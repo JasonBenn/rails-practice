3e6f9a4358d55f7a7edcc08b44afe207bc1cba23
Jason Benn <jasoncbenn@gmail.com>
2015-01-13 23:42:16 -0800

    Create, show.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,16 @@
 class TodosController < ApplicationController
+  def create
+    @todo = Todo.create(params.require(:todo).permit(:title, :description))
+    @todo.save
+    redirect_to @todo
+  end
+
   def new
     @todo = Todo.new
+  end
+
+  def show
+    # render plain: params.inspect
+    @todo = Todo.find_by_id(params[:id])
   end
 end
